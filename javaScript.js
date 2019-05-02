3c3d18c0008b67498d65dfe07dba69e8eecf2f4b
William Ballard <jacob.ballard.1999@gmail.com>
2019-05-02 13:44:17 -0400

    1.0

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,28 @@
-var customerTypeP = d3.csv("revVSconvForAllNewReturning.csv")
-var customerDeviceDailyP = d3.csv("revVSconvForAllNewReturningDevices.csv")
-var customerBounceP = d3.csv("revVSconvForBounce.csv")
-var customerDeviceMonthlyP = d3.csv("revVSconvForMobileTabletDesktop.csv")
+var dataP = d3.csv("convVSstate.csv")
+var mapP = d3.json("https://d3js.org/us-10m.v1.json")
 
 Promise.all([customerTypeP, customerBounceP, customerDeviceDailyP, customerDeviceMonthlyP]).then(function(values){
-  var customerType = values
-  var customerDeviceDaily = values
-  var customerBounce = values
-  var customerDeviceMonthlyP = values
+  var data = values
+  var map = values
+  drawMap(map, data);
 })
+
+var drawMap = function(map, data){
+var svg = d3.select("svg");
+
+var path = d3.geoPath();
+
+
+
+  svg.append("g")
+      .attr("class", "states")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.states).features)
+    .enter().append("path")
+      .attr("d", path);
+
+  svg.append("path")
+      .attr("class", "state-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+});
+}
